eef4312402f4fc6876416d20084ec59e7c4502fe
jaehui <jaehuigwak504@gmail.com>
2022-10-06 09:31:51 +0900

    add character health

---
+++ Assets/3. Scripts/CharacterHealth.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CharacterHealth : MonoBehaviour ,IDamagable
+{
+    [SerializeField] Slider healthSlider;
+    [SerializeField] float maxHp = 100f;
+    [HideInInspector] public float hp { get; private set; }
+
+    [SerializeField] AudioClip deathClip;
+    [SerializeField] AudioClip hitClip;
+
+    private AudioSource audioPlayer;
+    [HideInInspector] public bool dead { get; private set; }
+    [HideInInspector] public event Action onDeath;
+
+
+    private void Awake()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        hp = maxHp;
+        healthSlider.value = hp;
+    }
+
+    public void OnDamage(float damage)
+    {
+        if(!dead)
+        {
+            audioPlayer.PlayOneShot(hitClip);
+            hp -= damage;
+        }
+
+        if (hp <= 0 && !dead)
+        {
+            Die();
+        }
+
+        healthSlider.value = hp;
+    }
+
+
+    public virtual void Die()
+    {
+        if (onDeath != null)
+        {
+            onDeath();
+        }
+
+        dead = true;
+        audioPlayer.PlayOneShot(deathClip);
+    }
+
+
+}
