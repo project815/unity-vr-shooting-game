ee0147610d2328c71b1c12de357208ae35c0b0f6
jaehuiGwak <jaehuigwak504@gmail.com>
2022-10-10 23:37:50 +0900

    add scene change

---
+++ Assets/3. Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//score, game success/fail
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gInstance;
+
+    //check player death
+    public bool PlayerOver { get; private set; }
+
+    //total kill enemy count
+    private int KillCount = 0;
+    //goal to kill enemy
+    [SerializeField] int KillGoal = 0;
+    
+    void Awake()
+    {
+        //singleton
+        if(gInstance==null)
+        {
+            gInstance = this;
+        }
+        else
+        {
+            if(gInstance!=this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        PlayerOver = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player death Action delegate
+        CharacterHealth player = FindObjectOfType<CharacterController>().GetComponent<CharacterHealth>();
+        player.onDeath += PlayerGameOver;
+
+        //enemy death Action delegate
+        CharacterHealth enemy = FindObjectOfType<Zombie>().GetComponent<CharacterHealth>();
+        enemy.onDeath += AddKillCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SceneLoad
+        if(!PlayerOver && KillCount>=KillGoal)
+        {
+            UIManager.uInstance.SceneChange();
+        }
+    }
+
+    public void AddKillCount()
+    {
+        if(!PlayerOver)
+        {
+            KillCount++;
+        }
+    }
+
+    public void PlayerGameOver()
+    {
+        if(!PlayerOver)
+        {
+            PlayerOver = true;
+        }
+    }
+    
+}
